0e9b1d63e4070e9521e47441ff94116a1951540e
David Callan <davepcallan@gmail.com>
2025-01-23 11:34:28 +0000

    Amend the default "ASP.NET Core Web API" project which comes with .NET to add SwaggerUI back in and amend launchSettings to automatically open the browser and open swagger by default

--- APIUIDemo/Program.cs
+++ APIUIDemo/Program.cs
@@ -19,6 +19,11 @@ namespace APIUIDemo
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+                });
             }
 
             app.UseHttpsRedirection();
